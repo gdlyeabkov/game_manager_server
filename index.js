babc86658c9b0c2df16dfbd2eb42c08e599f8595
gdlyeabkov <gdlyeabkov@gmail.com>
2022-04-14 14:21:20 +0300

    deploy

---
+++ index.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+const url = `mongodb+srv://glebClusterUser:glebClusterUserPassword@cluster0.fvfru.mongodb.net/odbcs?retryWrites=true&w=majority`;
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+
+mongoose.connect(url, connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
+const GameSchema = new mongoose.Schema({
+    name: String,
+    url: String,
+    image: String
+}, { collection : 'my_digital_distributtion_games' })
+    
+const GameModel = mongoose.model('GameModel', GameSchema)
+
+app.get('/api/games/get', (req, res) => {
+    let query = GameModel.find({  })
+    query.exec((err, games) => {
+        if (err) {
+            return res.json({ "status": "Error" })
+        } else {
+            return res.json({ games: games, status: 'OK' })
+        }
+    })
+})
+
+
+app.get('/api/games/create', (req, res) => {
+        
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Access-Token, X-Socket-ID, Content-Type");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+    const game = new GameModel({ name: req.query.name, url: req.query.url, image: req.query.image })
+    game.save(function (err) {
+        if (err) {
+            return res.json({ "status": "Error" })
+        } else {
+            return res.json({ "status": "OK" })
+        }
+    })
+
+})
+
+// app.get('**', (req, res) => { 
+
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Access-Token, X-Socket-ID, Content-Type");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+      
+//     return res.json({
+//         status: 'OK'
+//     })
+
+// })
+
+const port = 4000
+// const port = 8000
+
+app.listen(port)
